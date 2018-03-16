2e8293b78e11f31dffee3231605335c4dadd8c99
ahmbas <ahmed.bashir@dubizzle.com>
2018-03-16 18:38:16 +0400

    missing template

--- loadbalancer/files/loadbalancer.py
+++ loadbalancer/files/loadbalancer.py
@@ -3,7 +3,7 @@ import json
 from haproxystats import HAProxyServer
 
 def get_lb_stats():
-    haproxy = HAProxyServer('127.0.0.1:80')
+    haproxy = HAProxyServer('127.0.0.1:5000')
     print(haproxy.to_json())
 
 get_lb_stats()
